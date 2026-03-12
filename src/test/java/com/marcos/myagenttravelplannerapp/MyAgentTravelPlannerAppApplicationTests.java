0fc1fdc3e73fb70342ad950e5f1521a93c7d918a
marcos <marcoslopezmarinn@gmail.com>
2026-03-12 22:49:44 +0100

    update application configuration and enhance test conditions

--- src/test/java/com/marcos/myagenttravelplannerapp/MyAgentTravelPlannerAppApplicationTests.java
+++ src/test/java/com/marcos/myagenttravelplannerapp/MyAgentTravelPlannerAppApplicationTests.java
@@ -1,9 +1,11 @@
 package com.marcos.myagenttravelplannerapp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnabledIfEnvironmentVariable(named = "OPENAI_API_KEY", matches = ".+")
 class MyAgentTravelPlannerAppApplicationTests {
 
     @Test
